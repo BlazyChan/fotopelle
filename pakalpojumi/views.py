3abaa6d2a3a2207ca06b7b9ece0f2106fef4ecc0
BlazyChan <gundarspelle@inbox.lv>
2022-06-03 14:04:46 +0300

    Bilžu nosūtīšana uz Django izmantojot Ajax (bilžu noņemšana vēl nav pilnveidota)

--- pakalpojumi/views.py
+++ pakalpojumi/views.py
@@ -5,6 +5,16 @@ from .models import PakalpojumaVeids, Pasutijums, BilzuGalerija, Bilde
 from django.core.serializers.json import DjangoJSONEncoder
 from django.core import serializers
 from lietotaji.models import Fotografs
+import base64
+from django.core.files.base import ContentFile
+import io
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
+
+
+# Lapas "Pasūtīt" mainīgie:
+gala_bildes = None
+gala_galerija = None
 
 
 # Sākumlapa (pirmā lapa, ko redz atverot mājaslapu):
@@ -15,7 +25,14 @@ def sakumlapa(request):
 # Lapa, kur lietotājs var veikt pasūtījumus, jeb pasūtīt pakalpojumus:
 def pasutit(request):
     if request.user.is_anonymous is False:
-        if request.method == "POST":
+        global gala_bildes
+        global gala_galerija
+
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest' and request.method == "POST":
+            gala_bildes = request.POST.getlist('django_bildes[]')
+            form = PasutijumaVeidlapa()
+
+        elif request.method == "POST":
             form = PasutijumaVeidlapa(request.POST)
             if form.is_valid():
                 instance = form.save(commit=False)
@@ -24,19 +41,8 @@ def pasutit(request):
                 instance.save()
 
                 # Izveido jaunu bilžu galeriju:
-                bilzu_galerija = BilzuGalerija.objects.create(
+                gala_galerija = BilzuGalerija.objects.create(
                     nosaukums=str(instance.pakalpojuma_veids) + " - " + request.user.vards + " " + request.user.uzvards + " (" + str(instance.pasutijuma_datums) + ")", pasutijums=instance)
-
-                # Saglabā katru augšupielādēto bildi (no "bilžu izdrukas" pasūtījuma veida):
-                if instance.pakalpojuma_veids.nosaukums == "Bilžu izdruka":
-                    bildes = request.FILES.getlist("bildes")
-                    for bilde in bildes:
-                        Bilde.objects.create(
-                            atrasanas_vieta=str('lietotajs_{0}/{1}/'.format(str(request.user.epasts.replace("@", "_")), str(bilzu_galerija.id) + "_" + str(bilzu_galerija.nosaukums))),
-                            fails=bilde,
-                            lietotajs=request.user,
-                            bilzu_galerija=bilzu_galerija
-                        )
 
                 # Veiksmīga pasūtījuma izveidošanas gadījumā - lietotāju aizved uz norādīto lapu un izvada ziņu:
                 messages.success(request, "Pasūtījums tika veiksmīgi izveidots!")
@@ -47,6 +53,30 @@ def pasutit(request):
         vaicajums = PakalpojumaVeids.objects.filter()
         # Pārveido modeli par JSON simbolu virkni:
         vaicajums = serializers.serialize("json", vaicajums, cls=DjangoJSONEncoder)
+
+        # Ja abi mainīgie eksistē, tad izveido bildes objektus:
+        if gala_bildes and gala_galerija:
+            bilzu_galerija = gala_galerija
+            bildes = gala_bildes
+            for bilde in bildes:
+                base64_fails = bilde.split("base64,")[1]
+                satura_veids = bilde.split(";")[0].split("data:")[1].split("+")[0]
+                bildes_nosaukums = "bilde" + "." + satura_veids.split("/")[1]
+
+                f = io.BytesIO(base64.b64decode(base64_fails))
+                bilde = InMemoryUploadedFile(f, field_name='picture', name=bildes_nosaukums, content_type=satura_veids,
+                                             size=sys.getsizeof(f), charset=None)
+                # Izveido bildes objektu:
+                Bilde.objects.create(
+                    atrasanas_vieta=str('lietotajs_{0}/{1}/'.format(str(request.user.epasts.replace("@", "_")),
+                                                                    str(bilzu_galerija.id) + "_" + str(
+                                                                        bilzu_galerija.nosaukums))),
+                    fails=bilde,
+                    lietotajs=request.user,
+                    bilzu_galerija=bilzu_galerija
+                )
+            gala_galerija = None
+            gala_bildes = None
         return render(request, 'pasutit.html', {"form": form, "pakalpojumu_apraksti": vaicajums})
     else:
         return redirect("/")
